d766b388dbd22de99d16542ed1862b6004797aea
Elwing-Chou <birdfan8814@gmail.com>
2022-01-20 14:51:08 +0800

    Add files via upload

--- 1_yt.py
+++ 1_yt.py
@@ -7,4 +7,9 @@ if __name__ == "__main__":
     driver = Chrome()
     driver.get("https://www.youtube.com/watch?v=Z2Z9V-4DMGw")
     driver.find_element_by_class_name("ytp-play-button").click()
-    time.sleep(20)
+    time.sleep(3)
+    for req in driver.requests:
+        print(req.url)
+        if req.response is not None:
+            print(req.response.headers["Content-Type"])
+        print("-" * 30)
